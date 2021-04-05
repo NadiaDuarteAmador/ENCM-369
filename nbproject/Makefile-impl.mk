2a49f444b9d895840e5f3c7698964a574a6b830d
NadiaDuarteAmador <paulaamador02@outlook.com>
2021-04-05 08:14:33 -0500

    files

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Pic-Activity-7.X
+PROJECTNAME=PIC_Activity7.X
 
 # Active Configuration
 DEFAULTCONF=default
--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Pic-Activity-7.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Pic-Activity-7.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=pic-activity-7.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/PIC_Activity7.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=PIC_Activity7.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=picactivity7.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/pic-activity-7.x/bin
+makeDirectory ${TMPDIR}/picactivity7.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/pic-activity-7.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/picactivity7.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/pic-activity-7.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/picactivity7.x.tar *
 checkReturnCode
 
 # Cleanup
